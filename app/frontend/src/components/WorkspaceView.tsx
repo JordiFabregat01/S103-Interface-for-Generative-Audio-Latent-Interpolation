17c00a3a510e8fe507dc8ca9e848f7c6793d9c13
Ericmas-9 <eric.mas02@estudiant.upf.edu>
2026-05-27 09:39:36 +0200

    Fletxes millors

--- app/frontend/src/components/WorkspaceView.tsx
+++ app/frontend/src/components/WorkspaceView.tsx
@@ -325,7 +325,7 @@ const getEmoji = (name: string, filename = "") => {
 
   // HUMAN
   if (lower.includes("snowsteps")) return "🥾❄️";
-  if (lower.includes("footsteps")) return "👣";
+if (lower.includes("footsteps")) return "/Footsteps_icon.png";
   if (lower.includes("keyboard")) return "⌨️";
 
   return "🎵";
@@ -456,7 +456,7 @@ const selectedPathPoints = sortedClips
   .filter(Boolean);
 
 const selectedPath = selectedPathPoints
-  .map((point) => `${point!.px},${point!.py}`)
+  .map((point) => `${point!.px},${point!.py-2}`)
   .join(" ");
 
   const deleteClip = (id: number) => {
